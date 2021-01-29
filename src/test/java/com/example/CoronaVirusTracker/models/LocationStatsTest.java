45c2c0008ba946efe3489a0abb96ce1a1e1cf4c5
fluke00700 <teerapat.thiti@gmail.com>
2021-01-29 16:14:28 +0100

    2nd commit with tests

--- src/test/java/com/example/CoronaVirusTracker/models/LocationStatsTest.java
+++ src/test/java/com/example/CoronaVirusTracker/models/LocationStatsTest.java
@@ -2,21 +2,18 @@ package com.example.CoronaVirusTracker.models;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LocationStatsTest {
 
     @Test
-    @DisplayName("should return the string of a location containing stat in this order state,country,latestTotalCases")
+    @DisplayName("should return the string of a location containing stat in this order: state,country,latestTotalCases")
     void testReturnString() {
         LocationStats testStat = new LocationStats();
         testStat.setState("Bermuda");
         testStat.setCountry("United Kingdom");
         testStat.setLatestTotalCases(690);
-
         var expected = "LocationStats{state='Bermuda', country='United Kingdom', latestTotalCases=690}";
-
         assertEquals(expected, testStat.toString());
     }
 
